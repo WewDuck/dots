b2751afdcff6aed97f55b7721feecc225d2cf222
WewDuck <bondarets.jr@gmail.com>
2024-04-07 14:43:59 +0200

    this should fix it

--- nix/systems/base/configuration.nix
+++ nix/systems/base/configuration.nix
@@ -20,7 +20,7 @@
    i18n.defaultLocale = "en_US.UTF-8";
    console = {
      font = "Lat2-Terminus16";
-     keyMap = "us";
+#    keyMap = "us";
      useXkbConfig = true; # use xkb.options in tty.
    };
    i18n.extraLocaleSettings = {
