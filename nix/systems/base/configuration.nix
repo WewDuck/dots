88e5b627b8c3e9e235d39e7c135e87c5ff1f560e
WewDuck <bondarets.jr@gmail.com>
2024-04-07 16:33:16 +0200

    ...

--- nix/systems/base/configuration.nix
+++ nix/systems/base/configuration.nix
@@ -18,11 +18,11 @@
 
   # Select internationalisation properties.
    i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+   console = {
+     font = "Lat2-Terminus16";
+     keyMap = "us";
+     useXkbConfig = true; # use xkb.options in tty.
+   };
    i18n.extraLocaleSettings = {
 	LC_ADDRESS = "nl_NL.UTF-8";
 	LC_IDENTIFICATION = "nl_NL.UTF-8";
