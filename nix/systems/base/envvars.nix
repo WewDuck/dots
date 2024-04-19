27a399dae06a380e717fc3cf901d5e909286c1a0
WewDuck <bondarets.jr@gmail.com>
2024-04-19 17:10:12 +0200

    change envvars

--- nix/systems/base/envvars.nix
+++ nix/systems/base/envvars.nix
@@ -1,15 +1,15 @@
 { config, pkgs, ... }:
 {
   # Declare Session Variables
-  environment.sessionVariables = {
-#   TERM = "xterm-256color";
-    EDITOR = "neovim";
- };
+# environment.sessionVariables = {
+#  TERM = "xterm-256color";
+#   EDITOR = "neovim";
+#};
 
  # Declare Environment Variables
  environment.variables = {
-#   TERM = "xterm-256color";
-    EDITOR = "neovim";
+   TERM = "xterm-256color";
+   EDITOR = "neovim";
  };
 }
 
