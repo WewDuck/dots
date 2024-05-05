7420792c72abf2324a5fe1b58833535079c337b1
WewDuck <bondarets.jr@gmail.com>
2024-05-05 23:09:37 +0200

    I will commit crimes

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -6,8 +6,8 @@
 	/home/ib/dots/modules/system/gaming.nix
 	/home/ib/dots/modules/system/localization.nix
 	/home/ib/dots/modules/system/nvidia.nix
-	/home/ib/dots/users/ib/home.nix
-	/home/ib/dots/modules/hm/themes/default.nix
+#	/home/ib/dots/users/ib/home.nix
+#	/home/ib/dots/modules/hm/themes/default.nix
 	./hardware-configuration.nix
 	./boot.nix
       ];
