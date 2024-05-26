91bdb2aff2548f2c5b9837c30fbd33171d4235ce
WewDuck <bondarets.jr@gmail.com>
2024-05-26 20:16:46 +0200

    disable h-m

--- users/ib/home.nix
+++ users/ib/home.nix
@@ -1,54 +1,89 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
-  
-  users.ib = {
+ users.users.ib = {
     isNormalUser = true;
     extraGroups = [ "wheel" "libvirt" ];
-  }
-
- programs.home-manager.enable = true;
- home = {
-   username = "ib";
-   homeDirectory = "/home/ib"; 
-   stateVersion = "23.11"; 
-   packages = with pkgs; [
-          armcord
-          firefox
-          floorp
-          neofetch
-          hyfetch
-          cava
-          cbonsai
-          cmatrix
-          lavat
-          ookla-speedtest
-          cfspeedtest
-          pfetch
-          kate
-          kcolorchooser
-          vlc
-          via
-          rpi-imager
-          wl-clipboard
-          xclip
-          tldr
-          typioca
-          alacritty
-          foot
-          kitty
-          krita
-          rofi-wayland
-          nicotine-plus
-          kdenlive
-          transmission-qt
-          transmission
-          obs-studio
-
+    packages = with pkgs; [
+         armcord
+         vesktop
+         firefox
+         floorp
+         neofetch
+         hyfetch
+         cava
+         cbonsai
+         cmatrix
+         lavat
+         ookla-speedtest
+         cfspeedtest
+         pfetch
+         kate
+         kcolorchooser
+         vlc
+         via
+         rpi-imager
+         wl-clipboard
+         xclip
+         tldr
+         typioca
+         alacritty
+         foot
+         kitty
+         krita
+         rofi-wayland
+         nicotine-plus
+         kdenlive
+         transmission-qt
+         transmission
+         obs-studio
     ];
   };
 
-  imports = [
-    ~/dots/modules/hm/themes/default.nix
-  ];
+# programs.home-manager.enable = true;
+#home-manager.users.ib = { pkgs, ... }: {
+#home = {
+#  username = "ib";
+#  homeDirectory = "/home/ib"; 
+#  stateVersion = "23.11"; 
+#  packages = with pkgs; [
+#         armcord
+#         firefox
+#         floorp
+#         neofetch
+#         hyfetch
+#         cava
+#         cbonsai
+#         cmatrix
+#         lavat
+#         ookla-speedtest
+#         cfspeedtest
+#         pfetch
+#         kate
+#         kcolorchooser
+#         vlc
+#         via
+#         rpi-imager
+#         wl-clipboard
+#         xclip
+#         tldr
+#         typioca
+#         alacritty
+#         foot
+#         kitty
+#         krita
+#         rofi-wayland
+#         nicotine-plus
+#         kdenlive
+#         transmission-qt
+#         transmission
+#         obs-studio
+#
+#   ];
+# };
+#};
+
+#  imports = [
+#    ../../modules/hm/themes/default.nix
+# ];
 
 }
