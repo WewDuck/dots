3820a8cba5e9bed54bcb2e3454a913adb2a15615
WewDuck <bondarets.jr@gmail.com>
2024-05-30 15:41:20 +0200

    sway moment

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -3,14 +3,14 @@
     # Imports
       imports = [
 	../../modules/system/etc.nix
-	../../modules/system/kde.nix
+#../../modules/system/kde.nix
 	../../modules/system/gaming.nix
 	../../modules/system/localization.nix
       	../../modules/system/nvidia.nix
 	../../modules/system/shell.nix
 	../../modules/system/xserver.nix
 	../../modules/system/audio.nix
-#	../../modules/system/sway.nix
+	../../modules/system/sway.nix
 	../../users/ib/home.nix
 #	/home/ib/dots/users/ib/home.nix
 #	/home/ib/dots/modules/hm/themes/default.nix
