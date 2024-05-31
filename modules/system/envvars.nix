96bfd496d478ca4c6c14c99de28e4c1454636f35
WewDuck <bondarets.jr@gmail.com>
2024-05-31 16:11:05 +0200

    disable vulkan

--- modules/system/envvars.nix
+++ modules/system/envvars.nix
@@ -5,7 +5,7 @@
   EDITOR = "neovim";
   WLR_NO_HARDWARE_CURSORS = "1";
   QT_QPA_PLATFORMTHEME = "qt5ct";
-  WLR_RENDERER = "vulkan";
+# WLR_RENDERER = "vulkan";
 };
 
  # Declare Environment Variables
@@ -14,7 +14,7 @@
    TERM = "xterm-256color";
    WLR_NO_HARDWARE_CURSORS = "1";
    QT_QPA_PLATFORMTHEME = "qt5ct";
-   WLR_RENDERER = "vulkan";
+#  WLR_RENDERER = "vulkan";
  };
 }
 
