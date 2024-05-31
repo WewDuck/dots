105052a87e9e166fedaa55827fa4257e2d144897
WewDuck <bondarets.jr@gmail.com>
2024-05-31 14:45:09 +0200

    lightdm

--- modules/system/xserver.nix
+++ modules/system/xserver.nix
@@ -14,6 +14,16 @@
    services.xserver.xkb.layout = "us";
 
 
-  services.xserver.displayManager.gdm.enable = true;
+# services.xserver.displayManager.gdm.enable = true;
+services.xserver.displayManager = {
+
+lightdm = {
+   enable = true;
+   greeters.slick.enable = true;
+   };
+
+setupCommands = "export WLR_NO_HARDWARE_CURSORS=1";
+};
+
 
 }
