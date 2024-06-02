80ea7fa7a0e6bb200653d2ce56a6615b585e6f0c
WewDuck <bondarets.jr@gmail.com>
2024-06-02 14:10:08 +0200

    readd kde

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -4,15 +4,15 @@
       imports = [
 	../../modules/system/etc.nix
 	../../modules/system/envvars.nix
-#../../modules/system/kde.nix
+	../../modules/system/kde.nix
 	../../modules/system/gaming.nix
 	../../modules/system/localization.nix
-#     	../../modules/system/nvidia.nix
+     	../../modules/system/nvidia.nix
 	../../modules/system/shell.nix
 	../../modules/system/xserver.nix
 	../../modules/system/audio.nix
-	../../modules/system/sway.nix
-	../../modules/system/hyprland.nix
+#	../../modules/system/sway.nix
+#	../../modules/system/hyprland.nix
 	../../users/ib/home.nix
 #	/home/ib/dots/users/ib/home.nix
 #	/home/ib/dots/modules/hm/themes/default.nix
