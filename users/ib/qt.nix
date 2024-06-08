b6337e7fd927d790758ab296fc9c26351e50dc30
WewDuck <bondarets.jr@gmail.com>
2024-06-08 23:54:06 +0200

    qt cat

--- users/ib/qt.nix
+++ users/ib/qt.nix
@@ -1,8 +1,9 @@
- { ... }:
+ { pkgs, ... }:
 {
     qt = {
         enable = true;
         platformTheme.name = "gtk";
-        style.name = "gtk2";
+        style.name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+        style.package = pkgs.catppuccin;
     };
 }
