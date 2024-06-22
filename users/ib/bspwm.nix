bc2a9c0607914b896e7667ac3ce2782d3aa4810c
WewDuck <bondarets.jr@gmail.com>
2024-06-23 00:01:36 +0200

    some stuffs

--- users/ib/bspwm.nix
+++ users/ib/bspwm.nix
@@ -5,14 +5,14 @@ xsession.windowManager.bspwm = {
     enable = true;
     extraConfigEarly = " pgrep -x sxhkd > /dev/null || sxhkd &
     ";
-#  monitors = {
-#      DP-1 = [
-#        "1"
-#        "2"
-#        "3"
-#        "4"
-#      ];
-#    };
+  monitors = {
+      DP-1 = [
+        "1"
+        "2"
+        "3"
+        "4"
+      ];
+    };
     settings = {
         border_width = 2;
         window_gap = 10;
