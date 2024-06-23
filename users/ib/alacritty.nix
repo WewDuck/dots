13fbb3b9d8b238f5e2ae5f0d2591a546816f21ec
WewDuck <bondarets.jr@gmail.com>
2024-06-23 17:37:36 +0200

    re-enable catppuccin for alacritty

--- users/ib/alacritty.nix
+++ users/ib/alacritty.nix
@@ -24,53 +24,53 @@ programs.alacritty = {
         style = "Bold";
         };  
       };
-#    colors = {
+    colors = {
         # Catppuccin Mocha theme
-#       primary = {
-#         background = "#181825";
-#         foreground = "#cdd6f4";
-#       };
-#       cursor = {
-#         text = "#1e1e2e";
-#         cursor = "#f5e0dc";
-#       };
-#       vi_mode_cursor = {
-#         text = "#1e1e2e";
-#         cursor = "#b4befe"; };
-#       selection = {
-#         text = "#1e1e2e";
-#         background = "#f5e0dc";
-#       };
-#       normal = {
-#         black = "#45475a";
-#         red = "#f38ba8";
-#         green = "#a6e3a1";
-#         yellow = "#f9e2af";
-#         blue = "#89b4fa";
-#         magenta = "#f5c2e7";
-#         cyan = "#94e2d5";
-#         white = "#bac2de"; };
-#       bright = {
-#         black = "#585b70";
-#         red = "#f38ba8";
-#         green = "#a6e3a1";
-#         yellow = "#f9e2af";
-#         blue = "#89b4fa";
-#         magenta = "#f5c2e7";
-#         cyan = "#94e2d5";
-#         white = "#a6adc8";
-#       };
-#       dim = {
-#         black = "#45475a";
-#         red = "#f38ba8";
-#         green = "#a6e3a1";
-#         yellow = "#f9e2af";
-#         blue = "#89b4fa";
-#         magenta = "#f5c2e7";
-#         cyan = "#94e2d5";
-#         white = "#bac2de";
-#       };
-#    };
+       primary = {
+         background = "#181825";
+         foreground = "#cdd6f4";
+       };
+       cursor = {
+         text = "#1e1e2e";
+         cursor = "#f5e0dc";
+       };
+       vi_mode_cursor = {
+         text = "#1e1e2e";
+         cursor = "#b4befe"; };
+       selection = {
+         text = "#1e1e2e";
+         background = "#f5e0dc";
+       };
+       normal = {
+         black = "#45475a";
+         red = "#f38ba8";
+         green = "#a6e3a1";
+         yellow = "#f9e2af";
+         blue = "#89b4fa";
+         magenta = "#f5c2e7";
+         cyan = "#94e2d5";
+         white = "#bac2de"; };
+       bright = {
+         black = "#585b70";
+         red = "#f38ba8";
+         green = "#a6e3a1";
+         yellow = "#f9e2af";
+         blue = "#89b4fa";
+         magenta = "#f5c2e7";
+         cyan = "#94e2d5";
+         white = "#a6adc8";
+       };
+       dim = {
+         black = "#45475a";
+         red = "#f38ba8";
+         green = "#a6e3a1";
+         yellow = "#f9e2af";
+         blue = "#89b4fa";
+         magenta = "#f5c2e7";
+         cyan = "#94e2d5";
+         white = "#bac2de";
+       };
+    };
     };
   };
 }
