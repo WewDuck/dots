5753de83b1e7c6392b1d772fd2d41744082d143c
WewDuck <bondarets.jr@gmail.com>
2024-06-27 17:17:56 +0200

    enable samba service for oldpc

--- hosts/oldpc/config.nix
+++ hosts/oldpc/config.nix
@@ -17,15 +17,9 @@
 
    time.timeZone = "Europe/Amsterdam";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
    i18n.defaultLocale = "en_US.UTF-8";
    console = {
-     font = "Lat2-Terminus16";
      keyMap = "us";
-#    useXkbConfig = true; # use xkb.options in tty.
    };
 
    users.users = {
