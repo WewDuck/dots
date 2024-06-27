f036a3837d369228920eb1887e0d38793b9fc72d
WewDuck <bondarets.jr@gmail.com>
2024-06-27 18:29:14 +0200

    samba share config

--- hosts/oldpc/samba.nix
+++ hosts/oldpc/samba.nix
@@ -1,7 +1,27 @@
-{...}
+{...}:
 {
 
 services.samba = {
 	enable = true;		
+	securityType = "user";
+	openFirewallv = true;
+	extraConfig = ''
+		workgroup = WORKGROUP
+		server string = oldpc
+		netbios name = oldpc
+		security = user
+	''
+	shares = {
+		private = {
+			path = "/mnt/samba/private";
+			browsable = "yes";
+			"read only" = "no";
+			"guest ok" = "no";
+			"create mask" = "0644";
+			"directory mask" = "0755";
+			"force group" = "sambaers"; 
+		};
+	};
+
 };
 }
