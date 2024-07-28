0140cd927a8ab5a63531867b548368e78ebbb99f
WewDuck <bondarets.jr@gmail.com>
2024-07-28 13:58:08 +0200

    Cinnamon

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -4,15 +4,15 @@
       imports = [
 	../../modules/etc.nix
 	../../modules/envvars.nix
-	../../modules/kde.nix
-	../../modules/kde-x11.nix
+#  ../../modules/kde.nix
+#	../../modules/kde-x11.nix
 	../../modules/gaming.nix
 	../../modules/localization.nix
-     	../../modules/nvidia.nix
+	../../modules/nvidia.nix
 	../../modules/shell.nix
 	../../modules/xserver.nix
 	../../modules/audio.nix
-#../../modules/cinnamon.nix
+  ../../modules/cinnamon.nix
 	../../modules/nh.nix
 	../../modules/virtualisation.nix
 #	../../modules/system/sway.nix
