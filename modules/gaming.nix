22ec601c975f6c7c547bed351f34734673b013e7
WewDuck <bondarets.jr@gmail.com>
2024-08-23 14:22:55 +0200

    opengl -> graphics

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -13,10 +13,10 @@
   };
 
   # Enable OpenGL
-   hardware.opengl = {
+   hardware.graphics = {
     enable = true;
 #   driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   # Vaapi
