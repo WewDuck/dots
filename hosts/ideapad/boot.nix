a4e64ec10617b05f5d723a948fadf048ef44725e
WewDuck <bondarets.jr@gmail.com>
2024-10-23 19:33:16 +0200

    add ideapad profiles

---
+++ hosts/ideapad/boot.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+let
+  grubTheme = pkgs.stdenv.mkDerivation {
+    pname = "distro-grub-themes";
+    version = "3.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "AdisonCavani";
+      repo = "distro-grub-themes";
+      rev = "v3.1";
+      hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+    };
+    installPhase = "cp -r customize/nixos $out";
+  };
+in
+{
+  boot.loader = { 
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    grub ={
+      enable = true;
+      devices = ["nodev"];
+      efiSupport = true;
+      useOSProber = true;
+      gfxmodeEfi = "2880x1800";
+      theme = grubTheme;
+    };
+  };
+
+    # Set systemd timeout
+systemd.extraConfig = "
+  DefaultTimeoutStopSec=10s
+  DefaultTimeoutStartSec=10s
+";
+  
+  # Set Linux Kernel Version.
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+}
+
---
+++ hosts/ideapad/default.nix
@@ -0,0 +1,24 @@
+{...}:
+{
+  imports = [
+    ../../modules/etc.nix
+    ../../modules/envvars.nix
+    ../../modules/kde.nix
+    ../../modules/kde-x11.nix
+    ../../modules/gaming.nix
+    ../../modules/localization.nix
+    ../../modules/shell.nix
+    ../../modules/xserver.nix
+    ../../modules/audio.nix
+    ../../nh.nix
+    ../../virtualisation.nix
+    ./hardware-configuration.nix
+    ./boot.nix
+  ];
+
+
+   networking.hostName = "ipad";
+   
+   system.stateVersion = "24.05";
+
+}
