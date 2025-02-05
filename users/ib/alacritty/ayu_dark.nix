cc615cff02f83812955f14c2438b1d9e879f1840
WewDuck <bondarets.jr@gmail.com>
2025-02-05 15:49:26 +0100

    add ayu_dark

---
+++ users/ib/alacritty/ayu_dark.nix
@@ -0,0 +1,49 @@
+programs.alacritty.settings.colors = {
+    # Ayu Dark theme
+    primary = {
+        background = "#0A0E14";
+        foreground = "#B3B1AD";
+    };
+    cursor = {
+        text = "#01060E";
+        cursor = "#FAE994";
+    };
+    vi_mode_cursor = {
+        text = "#01060E";
+        cursor = "#FAE994";
+    };
+    selection = {
+        text = "#01060E";
+        background = "#FAE994";
+    };
+    normal = {
+        black = "#01060E";
+        red = "#EA6C73";
+        green = "#91B362";
+        yellow = "#F9AF4F";
+        blue = "#53BDFA";
+        magenta = "#FAE994";
+        cyan = "#90E1C6";
+        white = "#C7C7C7";
+    };
+    bright = {
+        black = "#686868";
+        red = "#F07178";
+        green = "#C2D94C";
+        yellow = "#FFB454";
+        blue = "#59C2FF";
+        magenta = "#FFEE99";
+        cyan = "#95E6CB";
+        white = "#FFFFFF";
+    };
+    dim = {
+        black = "#01060E";
+        red = "#EA6C73";
+        green = "#91B362";
+        yellow = "#F9AF4F";
+        blue = "#53BDFA";
+        magenta = "#FAE994";
+        cyan = "#90E1C6";
+        white = "#C7C7C7";
+    };
+};
