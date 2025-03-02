77d6c4763a231052ba7117a9b4c2dfd3b48c4f08
WewDuck <bondarets.jr@gmail.com>
2025-03-02 16:44:52 +0100

    fix

--- modules/shell.nix
+++ modules/shell.nix
@@ -6,14 +6,14 @@
   users.users.ib.shell = pkgs.fish;
 
   # Fish shell
-#  programs.fish = {
-#    enable=true;
+  programs.fish = {
+    enable=true;
 #    shellAliases ={
 #      l = "ls -Alh";
 #      ls = "ls --color=auto";
 #      nf = "neofetch";
 #     };
-#    };
+    };
 
   environment.systemPackages = with pkgs; [
     sqlite
